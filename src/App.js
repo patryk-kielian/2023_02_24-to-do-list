0327d12a53c67177adfee525cc8a6300128c5b91
Patryk <patrykK1028@gmail.com>
2023-02-24 17:02:57 +0100

    placeholder JSX, basic styles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
 export default function App() {
-  return <div>bruh</div>;
+  return (
+    <>
+      <div className="container">
+        <h1 className="title">React To-Do List</h1>
+        <div className="controls-group">
+          <button className="control-add">Add a new to-do</button>
+          <div>
+            <button className="control-display selected">All</button>
+            <button className="control-display">To-Do</button>
+            <button className="control-display">Completed</button>
+          </div>
+        </div>
+        <div className="content-group"></div>
+      </div>
+      <div className="copyright">Patryk Kielian © 2023 all rights reserved</div>
+    </>
+  );
 }
